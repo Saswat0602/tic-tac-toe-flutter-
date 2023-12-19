b10707a8d9e7f9e0dec5d45b5e064fbb80418eab
saswat <saswatranjan0602@gmail.com>
2023-12-20 00:24:56 +0530

    tic-tac-toe(flutter)

---
+++ lib/constants/colors.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class MainColor {
+  static Color primaryColor = Color(0xFF1a936f);
+  static Color secondaryColor = Color.fromARGB(255, 255, 255, 255);
+  static Color accentColor = Color(0xFF4169e8);
+}
